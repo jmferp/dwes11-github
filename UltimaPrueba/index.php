badea1afaa4b18d20e21aeb1855aceea36f44ea8
Alumno <Alumno@W7Bsica>
2017-10-17 14:22:59 +0200

    ULTIMA

--- UltimaPrueba/index.php
+++ UltimaPrueba/index.php
@@ -1,3 +1,10 @@
 <?php
 echo  "<h1>Funciona</h1>";
+$nombre="Jose";
+echo '<h3>Mi nombre es '. $nombre .' </h3>';
+echo "<h3>Probamos con ",$nombre," el comportamiento de echo \n </h3>";
+define("ALUMNO","Jose Maria");
+echo "<p>El alumno es ".ALUMNO."</p>";
+echo "<p>El alumno es ".Alumno."</p>";
+
 ?>
