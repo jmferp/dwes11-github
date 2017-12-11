203f99198a31e51dd608aae10d17ee8b39fae986
Alumno <Alumno@W7Bsica>
2017-12-11 11:16:48 +0100

    alta

--- U3P04-AutenticacionPHP/login/alta.php
+++ U3P04-AutenticacionPHP/login/alta.php
@@ -1,4 +1,11 @@
 <?php
+$servidor = "localhost";
+$usuario = "alumno";
+$clave = "alumno";
+$conexion = new mysqli($servidor,$usuario,$clave,"catalogo11");
+$conexion->query("SET NAMES 'UTF8'");
+$mensajeError="";
+
 if (!session_status () == PHP_SESSION_NONE){
     session_name("idsesion11");
     session_start();
@@ -10,7 +17,26 @@ if (!session_status () == PHP_SESSION_NONE){
     if(isset($_POST['enviar'])){
         session_name('idsesion11');
         session_start ();
-        $_SESSION['name']=$_POST['nombre'];
+        
+        if ($_POST['usuario']==""){
+            $mensajeError="Error usuario";
+        }else{
+            if($_POST['password']==""){
+                $mensajeError="Error contraseña";
+            }else{
+                $usu=$_POST['usuario'];
+                $resultado = $conexion -> query("SELECT login FROM usuario where login='$usu'");
+                if($resultado->num_rows === 0){
+                    $_SESSION['name']=$_POST['usuario'];
+                    $conexion -> query("INSERT INTO usuario VALUES ($usu,$pas,$nom,$admin,$desc)");
+                }else{
+                    $mensajeError="Usuario ya existe";
+                }
+        }
+            
+            
+        }
+        
         header('location:index.php');
     }
     
@@ -26,8 +52,9 @@ if (!session_status () == PHP_SESSION_NONE){
 	Usuario:<input type="text" name="usuario">
 	Nombre:<input type="text" name="nombre">
 	Contraseña:<input type="text" name="password">
-	
-	
+	Descripcion:<input type="text" name="descripcion">
+	Tipo cuenta:<input type="radio" value="Administrador" name="admin">Administador
+	<input type="radio" value="Estandar" name="admin">Estandar
     <input type="submit" name="enviar">
 	</form> 
 </body>
