0d07acb47e56d091ce00db1a0b05ab6bb7bbaab4
Alumno <Alumno@W7Bsica>
2018-01-26 12:23:13 +0100

    redir

--- U4P06-Redireccion y reenvio/src/servlets/GenerarColoresServlet.java
+++ U4P06-Redireccion y reenvio/src/servlets/GenerarColoresServlet.java
@@ -1,10 +1,8 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class GenerarNumeroServlet
+ * Servlet implementation class GenerarColoresServlet
  */
-@WebServlet("/Sorpresa")
-public class GenerarNumeroServlet extends HttpServlet {
+@WebServlet("/GenerarColores")
+public class GenerarColoresServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public GenerarNumeroServlet() {
+    public GenerarColoresServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,18 +28,15 @@ public class GenerarNumeroServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setContentType("text/html;charset=UTF-8");
-		PrintWriter out = response.getWriter();
-		double numero=Math.round(Math.random()*100+1);
-		request.setAttribute("numero", numero);
-		RequestDispatcher rd=request.getRequestDispatcher("/GenerarColores");
+
+		double num1=Math.round(Math.random()*255+1);
+		request.setAttribute("num1", num1);
+		double num2=Math.round(Math.random()*255+1);
+		request.setAttribute("num2", num2);
+		double num3=Math.round(Math.random()*255+1);
+		request.setAttribute("num3", num3);
+		RequestDispatcher rd=request.getRequestDispatcher("/MostrarNumero");
 		rd.forward(request, response);
-		//ServletContext contexto = getServletContext();
-		//response.sendRedirect(contexto.getContextPath()+"/MostrarNumero");
-		out.println("<html><head><meta charset='UTF-8'/><title>"+numero+"</title></head>");
-		out.println("<p>"+numero+"</p>");
-		out.close();
 	}
 
 	/**
--- U4P06-Redireccion y reenvio/src/servlets/MostrarNumeroServlet.java
+++ U4P06-Redireccion y reenvio/src/servlets/MostrarNumeroServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -32,8 +33,23 @@ public class MostrarNumeroServlet extends HttpServlet {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		PrintWriter out = response.getWriter();
 		out.println("<html><head><meta charset='UTF-8'/><title>"+request.getAttribute("numero")+"</title></head>");
+		if(request.getAttribute("numero")==null) {
+			ServletContext contexto = getServletContext();
+			response.sendRedirect(contexto.getContextPath()+"/Sorpresa");
+			out.println("<p>Error: No se ha generado el numero</p>");
+		}else {
+			Object o=request.getAttribute("num1");
+			String r=o.toString();
+			//int g=Integer.parseInt(request.getAttribute("num1").toString());
+			//int b=Integer.parseInt(request.getAttribute("num1").toString());
+			out.println("<p>"+r+"</p>");
+			//out.println("<p>"+g+"</p>");
+			//out.println("<p>"+b+"</p>");
+		//out.println("<body style='color:rgb("+r+","+g+","+b+");'>");
 		out.println("<p>"+request.getAttribute("numero")+"</p>");
+		}
 		out.println("<a href="+"./index.html"+">Index</a>");
+		out.print("</body>");
 		out.close();
 		
 	}
