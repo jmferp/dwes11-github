f01bfcf05dbfb6ef89d63a58adcf02dcedff0998
Alumno <Alumno@W7Bsica>
2018-02-19 10:58:59 +0100

    bb

--- U5P01-Java-BBDD/src/servlets/MostrarAnimalesServlet.java
+++ U5P01-Java-BBDD/src/servlets/MostrarAnimalesServlet.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MostrarAnimalesServlet
  */
-<<<<<<< HEAD
+
 @WebServlet("/MostrarAnimalesServlet")
 public class MostrarAnimalesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -80,77 +80,6 @@ public class MostrarAnimalesServlet extends HttpServlet {
 		  e.printStackTrace();
 		}
 		
-=======
-@WebServlet("/MostrarAnimales")
-public class MostrarAnimalesServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MostrarAnimalesServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletContext contexto = getServletContext();
-		response.setContentType("text/html;UTF-8");
-		PrintWriter out = response.getWriter();
-		out.println("<html><head><meta charset='UTF-8'/></head><body>");
-		
-		Connection conn = null;
-		Statement sentencia = null;
-		try {
-		  // Paso 1: Cargar el driver JDBC.
-		  Class.forName("org.mariadb.jdbc.Driver").newInstance();
-
-		  // Paso 2: Conectarse a la Base de Datos utilizando la clase Connection
-		  String userName = "alumno";
-		  String password = "alumno";
-		  String url = "jdbc:mariadb://localhost/animales";
-		  conn = DriverManager.getConnection(url, userName, password);
-
-		  // Paso 3: Crear sentencias SQL, utilizando objetos de tipo Statement
-		  sentencia = conn.createStatement();
-
-		  // Paso 4: Ejecutar la sentencia SQL a través de los objetos Statement
-		  String consulta = "SELECT * from animal";
-		  ResultSet rset = sentencia.executeQuery(consulta);
-
-		  // Paso 5: Mostrar resultados
-		  if (!rset.isBeforeFirst() ) {    
-			    out.println("<h3>No hay resultados</p>");
-			}
-		  
-		  while (rset.next()) {
-		    out.println("<p>" + rset.getString("nombre") + ", " + rset.getString("especie") + "</p>");
-		  }
-
-		  // Paso 6: Desconexión
-		  if (sentencia != null)
-		    sentencia.close();
-		  if (conn != null)
-		    conn.close();
-		} catch (Exception e) {
-		  e.printStackTrace();
-		}
-
->>>>>>> branch 'master' of https://github.com/jmferp/dwes11-github.git
-		
-		
 		out.println("</body></html>");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
