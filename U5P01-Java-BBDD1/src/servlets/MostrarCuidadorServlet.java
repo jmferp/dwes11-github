b908c08afeccc3116d31cd0a38892bcfd28c5f87
jmfer <jmfer@LAPTOP-S893HFLU>
2018-02-19 21:28:07 +0100

    cuidadores

--- U5P01-Java-BBDD1/src/servlets/MostrarCuidadorServlet.java
+++ U5P01-Java-BBDD1/src/servlets/MostrarCuidadorServlet.java
@@ -2,6 +2,10 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MostrarCuidadorServlet
  */
-@WebServlet("/MostrarCuidadorServlet")
+@WebServlet("/MostrarCuidador")
 public class MostrarCuidadorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -55,6 +59,52 @@ public class MostrarCuidadorServlet extends HttpServlet {
 		  out.println("<h3>Error: el identificador de cuidador debe ser numérico</h3>");
 		} else {
 		  // TO-DO: incluir el código de acceso a la base de datos
+			Connection conn = null;
+			Statement sentencia = null;
+			try {
+			  // Paso 1: Cargar el driver JDBC.
+			  Class.forName("org.mariadb.jdbc.Driver").newInstance();
+
+			  // Paso 2: Conectarse a la Base de Datos utilizando la clase Connection
+			  String userName = "alumno";
+			  String password = "alumno";
+			  String url = "jdbc:mariadb://localhost:3306/animales";
+			  conn = DriverManager.getConnection(url, userName, password);
+
+			  // Paso 3: Crear la sentencia SQL
+			  sentencia = conn.createStatement();
+
+			  // Paso 4: Ejecutar la sentencia SQL a través de los objetos Statement
+			  String consulta = "SELECT nombre from cuidador WHERE idCuidador="+idCuidador ;
+			  ResultSet rset = sentencia.executeQuery(consulta);
+
+			  // Paso 5: Mostrar resultados
+			  if (!rset.isBeforeFirst() ) {    
+			    out.println("<h3>Error: identificador de cuidador no válido</h3>");
+			  }
+			  else {
+			    rset.next();
+			    out.println("<p>Animales cuidados por " + rset.getString("nombre")+ ":</p>");
+			    // TO-DO: código para mostrar los animales
+			    String consultaAnimales = "SELECT animal.* FROM animal, cuida WHERE (animal.chip = cuida.chipAnimal) AND (cuida.idCuidador = '"+idCuidador+"')";
+			    rset = sentencia.executeQuery(consultaAnimales);
+			    if (!rset.isBeforeFirst() ) {    
+			      out.println("<p>Este cuidador no cuida ningún animal</p>");
+			    }
+			    while (rset.next()) {
+			      out.println("<li>" + rset.getString("nombre") + ", de la especie " + rset.getString("especie") + "</li>");
+			    }
+			    out.println("</ul>");
+			  }
+
+			  // Paso 6: Desconexión
+			  if (sentencia != null)
+			    sentencia.close();
+			  if (conn != null)
+			    conn.close();
+			} catch (Exception e) {
+			  e.printStackTrace();
+			}
 		}
 	}
 
