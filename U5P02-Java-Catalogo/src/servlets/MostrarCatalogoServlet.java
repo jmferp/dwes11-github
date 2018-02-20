fefb950e932aa828deec7a68f9e123cc3195389c
Alumno <Alumno@W7Bsica>
2018-02-20 14:23:09 +0100

    catal

---
+++ U5P02-Java-Catalogo/src/servlets/MostrarCatalogoServlet.java
@@ -0,0 +1,105 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MostrarCatalogo
+ */
+@WebServlet("/MostrarCatalogo")
+public class MostrarCatalogoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MostrarCatalogoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletContext contexto = getServletContext();
+		response.setContentType("text/html;UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><meta charset='UTF-8'/></head><body>");
+		
+		Connection conn = null;
+		Statement sentencia = null;
+		try {
+		  // Paso 1: Cargar el driver JDBC.
+		 Class.forName("org.mariadb.jdbc.Driver").newInstance();
+
+		  // Paso 2: Conectarse a la Base de Datos utilizando la clase Connection
+		  String userName = "alumno";
+		  String password = "alumno";
+		  String url = "jdbc:mariadb://localhost:3306/catalogo11";
+		  conn = DriverManager.getConnection(url, userName, password);
+
+		  // Paso 3: Crear la sentencia SQL
+		  sentencia = conn.createStatement();
+
+		  // Paso 4: Ejecutar la sentencia SQL a través de los objetos Statement
+		  String consulta = "SELECT titulo,nombre,imagen from obra,autor WHERE obra.id_autor=autor.id";
+		  ResultSet rset = sentencia.executeQuery(consulta);
+
+		  // Paso 5: Mostrar resultados
+		  if (!rset.isBeforeFirst() ) {    
+			    out.println("<h3>No hay resultados</p>");
+			}
+		  
+		  ArrayList arr=new ArrayList();
+		  Obra obra=new Obra(rset.getString("titulo"), consulta, consulta, consulta, consulta, consulta, consulta, consulta);
+		  
+		  out.println("<table style='border:'5px''>");
+		  out.println("<tr style='background-color:lightblue'><td>Titulo</td><td>Autor</td><td>Imagen</td></tr>");
+		  while (rset.next()) {
+			out.println("<tr style='background-color:orange'>");
+		    out.println("<td>" + rset.getString("titulo") + "</td><td> " + rset.getString("autor") + "</td><td><img src='./img/"+rset.getString("imagen")+"' width=100 heigh=100></td></tr>");
+		  }
+		  out.println("</table>");
+
+		  // Paso 6: Desconexión
+		  if (sentencia != null)
+		    sentencia.close();
+		  if (conn != null)
+		    conn.close();
+		} catch (Exception e) {
+		  e.printStackTrace();
+		}
+	
+	
+	
+		
+		
+		
+		
+		out.println("</body></html>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
